47f962a1d0f1cd4aec1d0938098bedc174e273bc
Dasith Wijesiriwardena <dasiths@hotmail.com>
2016-11-01 01:48:41 +1100

    Added comments to IEvent and Event calsses

--- EventSourcingDemo/Events/Event.cs
+++ EventSourcingDemo/Events/Event.cs
@@ -1,15 +1,43 @@
-﻿using System;
+﻿/****************************** Class Header ******************************\
+Module Name:    <Event.cs>
+Project:        <EventSourcingDemo> [https://github.com/dasiths/EventSourcingDemo]
+Author:         Dasith Wijesiriwardena [https://github.com/dasiths]
+
+This is the base class for all events. We implement the reqauired properties to make it work with Aggregates. 
+This has be Serializable to make it persist to storage and reload. 
+Important: Events are immutable by design. I've left the property set methods public because of potential deserialization issues.
+\***************************************************************************/
+
+using System;
 using EventSourcingDemo.Domain;
 
 namespace EventSourcingDemo.Events
 {
+
+    /// <summary>
+    /// The base class for all events applies to an AggregateRoot
+    /// </summary>
     [Serializable]
     public class Event : IEvent
     {
+        /// <summary>
+        /// Target version of the Aggregate this event will be applied against
+        /// </summary>
         public int TargetVersion { get; set; }
+        /// <summary>
+        /// The aggregateID of the aggregate
+        /// </summary>
         public Guid AggregateId { get;  set; }
+        /// <summary>
+        /// The Unique Event ID
+        /// </summary>
         public Guid Id { get; set; }
 
+        /// <summary>
+        /// This method makes it easier to contruct a new Event
+        /// </summary>
+        /// <param name="AggregateID">Aggregate ID</param>
+        /// <param name="TargetVersion">Target Version</param>
         protected void Setup(Guid AggregateID, int TargetVersion)
         {
             this.AggregateId = AggregateID;
--- EventSourcingDemo/Events/IEvent.cs
+++ EventSourcingDemo/Events/IEvent.cs
@@ -1,9 +1,23 @@
-﻿using System;
+﻿/****************************** Class Header ******************************\
+Module Name:    <IEvent.cs>
+Project:        <EventSourcingDemo> [https://github.com/dasiths/EventSourcingDemo]
+Author:         Dasith Wijesiriwardena [https://github.com/dasiths]
+
+Interface for all events in the system. Requires an unique eventID
+\***************************************************************************/
+
+using System;
 
 namespace EventSourcingDemo.Events
 {
+    /// <summary>
+    /// Interface for all events
+    /// </summary>
     public interface IEvent
     {
+        /// <summary>
+        /// Unique event ID
+        /// </summary>
         Guid Id { get; }
     }
 }
