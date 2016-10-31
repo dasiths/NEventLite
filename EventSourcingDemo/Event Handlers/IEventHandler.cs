46ae26b0ee1d32db90bde77cc3d134d5a14cc7b3
Dasith Wijesiriwardena <dasiths@hotmail.com>
2016-11-01 01:39:13 +1100

    added comments to IEventHandler

--- EventSourcingDemo/Event Handlers/IEventHandler.cs
+++ EventSourcingDemo/Event Handlers/IEventHandler.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/****************************** Class Header ******************************\
+Module Name:    <IEventHandler.cs>
+Project:        <EventSourcingDemo> [https://github.com/dasiths/EventSourcingDemo]
+Author:         Dasith Wijesiriwardena [https://github.com/dasiths]
+
+This simply has an Apply method to do state changes in the implemented AggregateRoot
+\***************************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,8 +15,16 @@ using EventSourcingDemo.Domain;
 
 namespace EventSourcingDemo.Event_Handlers
 {
+    /// <summary>
+    /// Interface to expose the Apply() of event T in an AggregateRoot
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface IEventHandler<T> where T: Events.Event
     {
+        /// <summary>
+        /// Apply the event
+        /// </summary>
+        /// <param name="event">Event</param>
         void Apply(T @event);
     }
 }
