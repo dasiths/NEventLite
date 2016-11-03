f066767c545ff20f585cb974bfecdcc320f24065
Dasith Wijesiriwardena <dasiths@hotmail.com>
2016-11-04 01:14:30 +1100

    Added empty constructor to Snapshot

--- NEventLite Framework/Snapshot/Snapshot.cs
+++ NEventLite Framework/Snapshot/Snapshot.cs
@@ -8,7 +8,12 @@ namespace NEventLite.Snapshot
         public Guid AggregateId { get; set; }
         public int Version { get; set; }
 
-        public Snapshot(Guid id, Guid aggregateId, int version)
+        public Snapshot()
+        {
+            
+        }
+
+        public Snapshot(Guid id, Guid aggregateId, int version):base()
         {
             Id = id;
             AggregateId = aggregateId;
