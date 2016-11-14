8777a8320c29f74468dd9577c3992f71d88132c3
Dasith@4463 <daiths@hotmail.com>
2016-11-14 17:00:58 +1100

    command bus interface added

---
+++ NEventLite Framework/Command Bus/ICommandBus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using NEventLite.Commands;
+
+namespace NEventLite.Command_Bus
+{
+    public interface ICommandBus
+    {
+        void Publish(ICommand command);
+    }
+}
