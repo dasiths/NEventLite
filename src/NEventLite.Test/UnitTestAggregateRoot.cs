d99bc9285ff5276bb6d65536f4c2f3738ea42ce3
Dasith Wijesiriwardena <dasiths@hotmail.com>
2017-01-04 19:13:38 +1100

    Added unit tests

---
+++ src/NEventLite.Test/UnitTestAggregateRoot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NEventLite.Domain;
+using NEventLite.Snapshot;
+using NEventLite_Example.Domain;
+
+namespace NEventLite.Test
+{
+    [TestClass]
+    public class UnitTestAggregateRoot
+    {
+        [TestMethod]
+        public void IsNoteAnAggregateRoot()
+        {
+            Assert.IsInstanceOfType(new Note(), typeof(AggregateRoot));
+        }
+
+        [TestMethod]
+        public void IsNoteSnapshottable()
+        {
+            Assert.IsInstanceOfType(new Note(), typeof(ISnapshottable));
+        }
+    }
+}
