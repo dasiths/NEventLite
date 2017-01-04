66e242fe3336cd01cf1e9590050076cd18453e98
Dasith Wijesiriwardena <dasiths@hotmail.com>
2017-01-04 19:30:36 +1100

    added initialization to unit tests

--- src/Unit Tests/UnitTestAggregateRoot.cs
+++ src/Unit Tests/UnitTestAggregateRoot.cs
@@ -3,12 +3,30 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NEventLite.Domain;
 using NEventLite.Snapshot;
 using NEventLite_Example.Domain;
+using NEventLite_Example.Util;
 
 namespace NEventLite.Test
 {
     [TestClass]
     public class UnitTestAggregateRoot
     {
+        private MyLifeTimeScope _scope;
+        private DependencyResolver _container;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _scope = new MyLifeTimeScope();
+            _container = new DependencyResolver();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            _container.Dispose();
+            _scope.Dispose();
+        }
+
         [TestMethod]
         public void IsNoteAnAggregateRoot()
         {
